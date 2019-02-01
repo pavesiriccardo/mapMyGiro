396ea4304d1c7322022b2d2dfa3931a76681455e
pavesiriccardo <pavesiriccardo3@gmail.com>
2019-01-31 18:44:46 -0800

    More retries fetching from static APIs.

--- flaskexample/backend.py
+++ flaskexample/backend.py
@@ -133,7 +133,7 @@ def empty_cutouts_folder():
 
 
 
-def requests_retry_session(retries=3,backoff_factor=0.3,status_forcelist=(500, 502, 504),session=None):
+def requests_retry_session(retries=10,backoff_factor=0.3,status_forcelist=(500, 502, 504),session=None):
     session = session or requests.Session()
     retry = Retry(total=retries,read=retries,connect=retries,backoff_factor=backoff_factor,status_forcelist=status_forcelist)
     adapter = HTTPAdapter(max_retries=retry)
