5279aa0a29392a3c6cfe8af402399d8cd8e87205
pavesiriccardo <pavesiriccardo3@gmail.com>
2019-02-04 09:49:56 -0800

    syntax fix

--- flaskexample/backend.py
+++ flaskexample/backend.py
@@ -164,7 +164,7 @@ def fetch_from_Google(list_of_coords,list_of_filenames):
         my_iterator=product(changes_to_try,changes_to_try)
         change_pair=next(my_iterator,None)
         while r.status_code==500 and change_pair is not None:
-        	    print('Im having to shift slightly by',change_pair)
+                print('Im having to shift slightly by',change_pair)
                 center = "{0:.6f}".format(lat+change_pair[0])+","+"{0:.6f}".format(long+change_pair[1])   
                 unsigned_url=url + "center=" + center + "&zoom=" +str(zoom) + "&size=640x640&format=jpg&maptype=satellite&key=" +api_key
                 signed_url=sign_url(input_url=unsigned_url,  secret=google_secret_key)
