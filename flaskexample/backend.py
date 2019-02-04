81b11facc7f6c00ae0d26b0e483958fe01ce5976
pavesiriccardo <pavesiriccardo3@gmail.com>
2019-02-04 09:47:28 -0800

    Fixing the error 500 by slight coordinate shifts

--- flaskexample/backend.py
+++ flaskexample/backend.py
@@ -11,6 +11,7 @@ from requests.packages.urllib3.util import parse_url
 import hashlib
 import hmac
 import base64
+from itertools import product
 
  
 
@@ -138,7 +139,7 @@ def empty_cutouts_folder():
 
 
 
-def requests_retry_session(retries=6,backoff_factor=0.4,status_forcelist=(500, 502, 504),session=None):
+def requests_retry_session(retries=3,backoff_factor=0.4,status_forcelist=(500, 502, 504),session=None):
     session = session or requests.Session()
     retry = Retry(total=retries,read=retries,connect=retries,backoff_factor=backoff_factor,status_forcelist=status_forcelist)
     adapter = HTTPAdapter(max_retries=retry)
@@ -158,13 +159,22 @@ def fetch_from_Google(list_of_coords,list_of_filenames):
         #r = requests.get(url + "center=" + center + "&zoom=" +str(zoom) + "&size=640x640&format=jpg&maptype=satellite&key=" +api_key)
         unsigned_url=url + "center=" + center + "&zoom=" +str(zoom) + "&size=640x640&format=jpg&maptype=satellite&key=" +api_key
         signed_url=sign_url(input_url=unsigned_url,  secret=google_secret_key)
-        r = requests_retry_session().get(signed_url) 
+        r = requests_retry_session().get(signed_url)
+        changes_to_try=[-1e-6,1e-6,2e-6,-2e-6,3e-6,-3e-6]
+        my_iterator=product(changes_to_try,changes_to_try)
+        change_pair=next(my_iterator,None)
+        while r.status_code==500 and change_pair is not None:
+                center = "{0:.6f}".format(lat+change_pair[0])+","+"{0:.6f}".format(long+change_pair[1])   
+                unsigned_url=url + "center=" + center + "&zoom=" +str(zoom) + "&size=640x640&format=jpg&maptype=satellite&key=" +api_key
+                signed_url=sign_url(input_url=unsigned_url,  secret=google_secret_key)
+                r = requests_retry_session().get(signed_url)
+                change_pair=next(my_iterator,None)
         try:
-        	r.raise_for_status()
+            r.raise_for_status()
         except:
         	print(signed_url)
         f = open(list_of_filenames[idx]+'.jpeg', 'wb') 
-        f.write(r.content) 
+        f.write(r.content)
         f.close() 
     #map(fetch_one_image,enumerate(list_of_coords))
     with Pool(30) as p:
